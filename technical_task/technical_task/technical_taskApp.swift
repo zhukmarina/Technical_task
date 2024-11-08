f2970f024532ec717c83778445e69c444ed44b11
Marina Zhukova <marinazhukova28@gmail.com>
2024-11-08 21:26:29 +0200

    initial commit

---
+++ technical_task/technical_task/technical_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  technical_taskApp.swift
+//  technical_task
+//
+//  Created by Marina Zhukova on 08.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct technical_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
