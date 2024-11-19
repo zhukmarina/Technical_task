9b6f1981e18f843edb22e281979b858aad625669
Marina Zhukova <marinazhukova28@gmail.com>
2024-11-19 16:38:06 +0200

    editing spacing in mainscreen

--- technical_task/technical_task/Views/Main/MainScreenView.swift
+++ technical_task/technical_task/Views/Main/MainScreenView.swift
@@ -17,70 +17,70 @@ struct MainScreenView: View {
     var body: some View {
         VStack(spacing: 16) {
             TextFieldforIP(ipAddress: $ipAddress, isValid: $isValid, clearIPAction: clearIP)
-            Button(action: {
-                validateIP(&ipAddress, isValid: &isValid)
-                if isValid == true {
+            VStack(spacing: 8) {
+                Button(action: {
+                    validateIP(&ipAddress, isValid: &isValid)
+                    if isValid == true {
+                        Task {
+                            isLoading = true
+                            await viewModel.fetchIPInfo(for: ipAddress)
+                            isLoading = false
+                        }
+                    } else {
+                        showingAlert = true
+                    }
+                }) {
+                    Text("Get Info")
+                        .font(.headline3)
+                        .lineSpacing(20)
+                        .foregroundColor(.basicWhite)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blueMain)
+                        .cornerRadius(10)
+                }
+                .alert(isPresented: $showingAlert) {
+                    Alert(
+                        title: Text("Alert error"),
+                        message: Text("Wrong IP! Please provide a valid IP address"),
+                        primaryButton: .default(Text("OK"), action: {
+                            clearIP()
+                        }),
+                        secondaryButton: .cancel(Text("Cancel"))
+                    )
+                }
+                
+                Button(action: {
                     Task {
                         isLoading = true
-                        await viewModel.fetchIPInfo(for: ipAddress)
+                        let fetchedIP = await viewModel.fetchUserIP()
+                        ipAddress = fetchedIP ?? ""
+                        isValid = true
                         isLoading = false
                     }
-                } else {
-                    showingAlert = true
+                }) {
+                    Text("Find Me")
+                        .font(.headline3)
+                        .foregroundColor(.blueMain)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blueMedium)
+                        .cornerRadius(10)
                 }
-            }) {
-                Text("Get Info")
-                    .font(.headline3)
-                    .lineSpacing(20)
-                    .foregroundColor(.basicWhite)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.blueMain)
-                    .cornerRadius(10)
-            }
-            .alert(isPresented: $showingAlert) {
-                Alert(
-                    title: Text("Alert error"),
-                    message: Text("Wrong IP! Please provide a valid IP address"),
-                    primaryButton: .default(Text("OK"), action: {
-                        clearIP()
-                    }),
-                    secondaryButton: .cancel(Text("Cancel"))
-                )
-            }
-            
-            Button(action: {
-                Task {
-                    isLoading = true
-                    let fetchedIP = await viewModel.fetchUserIP()
-                    ipAddress = fetchedIP ?? ""
-                    isValid = true
-                    isLoading = false
+                
+                Button(action: {
+                    clearIP()
+                }) {
+                    Text("Reset")
+                        .font(.headline3)
+                        .foregroundColor(.redMedium)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.redLight)
+                        .cornerRadius(10)
                 }
-            }) {
-                Text("Find Me")
-                    .font(.headline3)
-                    .foregroundColor(.blueMain)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.blueMedium)
-                    .cornerRadius(10)
             }
-            
-            Button(action: {
-                clearIP()
-            }) {
-                Text("Reset")
-                    .font(.headline3)
-                    .foregroundColor(.redMedium)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.redLight)
-                    .cornerRadius(10)
-            }
-        }
-        .padding()
-        
+
         if isLoading {
             ProgressView()
                 .padding()
@@ -93,6 +93,8 @@ struct MainScreenView: View {
                 .foregroundColor(.red)
                 .padding()
         }
+        }
+        .padding()
     }
     
     private func clearIP() {
